0c283e55bb11e1b89556fb13bbbc70a61500319d
tpfei <1144274779@qq.com>
2018-09-08 09:44:14 +0800

    delete this file

--- PartyMemberSystem/.svn/pristine/a9/a9217eeb661c2915de1bda24478ac770826b7c8d.svn-base
+++
@@ -1,7 +0,0 @@
-package org.lanqiao.dao;
-
-import org.lanqiao.bean.User;
-
-public interface LoginDao {
-	public User login(String username, String password);
-}
--- PartyMemberSystem/src/org/lanqiao/service/LoginService.java
+++
@@ -1,7 +0,0 @@
-package org.lanqiao.service;
-
-import org.lanqiao.bean.User;
-
-public interface LoginService {
-	public User login(String username,String password);
-}
